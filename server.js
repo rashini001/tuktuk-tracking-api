ae13fa4ee8e228c7f5fe53f1c2e8d844a01f2067
rashini001 <145641377+rashini001@users.noreply.github.com>
2026-04-07 17:22:44 +0530

    modify server files

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'TukTuk API is running!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
